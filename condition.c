67854cf10e37f98c4969095e198e88bcedcb557e
Princof <princof.thefreelancer@gmail.com>
2018-07-20 06:36:31 +0200

    memonto exo

---
+++ condition.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+
+	/*
+	int val =25;
+	int ent=0;
+	
+	printf("Bonjour dans ce jeu de comparaison \n");
+	printf("____________Veuillez saisir un chiffre_____________\n");
+	scanf("%d", &ent);
+	getchar();
+	
+	if(ent < val){
+		printf("le chifre %d est plus petit \n", ent);
+		
+	}else if (ent > val){
+		printf("Le chiffre %d est plus grand \n", ent);
+	}else{
+		puts("      vous avez Gagner     ");
+		puts("--------_____BRAVO____-------");
+		puts("  -----$$$$$$$$--------------");
+		puts("-------$$$$$**$$$$$------------------");
+		printf("vous avez trouver le chiffre %d \n", val);
+	}
+	
+	
+	*/
+	
+	
+/*--------------------------------------------------------------------------------------------------*/
+	
+	
+	
+	/*	
+	int a=0;
+	int b=1;
+	
+	printf("a et b %d \n", !(b));
+
+	*/	
+	
+	
+	
+	
+	
+/*--------------------------------------------------------------------------------------------------*/
+	
+	
+	/*int a=0;
+	int b=0;
+	
+	printf("Veuillez saisir un nombre \n");
+	scanf("%d", &a);
+	getchar();
+	
+	printf("Veuillez saisir un 2eme nombre \n");
+	scanf("%d",&b);
+	getchar();
+	
+	if(a<b){
+		
+		printf("la valeur %d est superieur \n", b);
+	}else{
+		printf("la valeur %d est superieur \n", a);
+		
+		}
+	*/
+	
+      return 0;	
+}
