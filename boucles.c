f64ba035be7a90cd94fea25531a1506aa9847b19
Princof <princof.thefreelancer@gmail.com>
2018-07-20 02:43:23 +0200

    frist_commit

---
+++ boucles.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<time.h>
+
+int main(){
+	int i=0;
+	printf("veuillez saisire une touche svp \n");
+	scanf("%d", &i);s
+	
+	while(i<25){
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	/*int var = time(NULL);
+	
+	printf("fonction time %d \n", var);*/
+	
+	/*------------------------------------------------------------------------------------*/
+	
+	
+	/*int nb_alea=0;
+	
+	srand(time (NULL));
+	
+	nb_alea = rand() % 1999900; 
+	
+	printf("Nombre aleatoire: %d \n ", nb_alea);
+	
+	*/
+	
+	
+	
+	
+	/*
+	int i =0;
+	
+	do{
+		printf(" i=%d \n", i);
+		i=i+1;
+	
+	}
+	while(i<10);
+	
+*/	
+	
+	
+	
+	
+	
+
+/*	char  caractere='   ';
+	int sortie =0;
+	
+	do{
+		printf("Appuyez su S pour sortire ! \n");
+		caractere = getchar();
+		
+		sortie = ((caractere == 's') || (caractere == 'S'));
+	}
+	
+	while (sortie == 0);*/
+	
+	return 0;
+}
